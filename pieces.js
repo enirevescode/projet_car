bf884df756b8fdf16ae536cea9886201ca5f6060
enirevescode <enirevescode@gmail.com>
2023-06-07 12:10:12 +0200

    boucle produits

--- pieces.js
+++ pieces.js
@@ -2,7 +2,8 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[0];
+for(let i=0; i<pieces.length; i++){
+const article = pieces[i];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
@@ -12,7 +13,7 @@ prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" 
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)";
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
 const disponibiliteElement = document.createElement("p");
 disponibiliteElement.innerText = article.disponibilite ? "En stock" : "(En Rupture de stock pour le moment.)";
 
@@ -24,3 +25,4 @@ sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(disponibiliteElement);
+}
